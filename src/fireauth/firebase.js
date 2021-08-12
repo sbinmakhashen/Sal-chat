732562fb6fc2d07dac6136d14b3d6fd30f772ce7
Salem <nlsabm@yahoo.com>
2021-08-12 00:14:22 -0400

    firebase config

--- src/fireauth/firebase.js
+++ src/fireauth/firebase.js
@@ -1,1 +1,12 @@
-import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+export const auth = firebase
+  .initializeApp({
+    apiKey: 'AIzaSyAaBeLQER0jE4-lL1hfTcuvGIiSd5BKupo',
+    authDomain: 'sal-chat.firebaseapp.com',
+    projectId: 'sal-chat',
+    storageBucket: 'sal-chat.appspot.com',
+    messagingSenderId: '634191124594',
+    appId: '1:634191124594:web:a1c48ba3912a0f9b71caeb',
+  })
+  .auth();
