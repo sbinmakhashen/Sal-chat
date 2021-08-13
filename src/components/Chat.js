e11e0511531248e27e5e9378e98fdc010719754a
Salem <nlsabm@yahoo.com>
2021-08-12 23:08:23 -0400

    handling logout

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { auth } from '../fireauth/firebase';
+import { useHistory } from 'react-router';
 
 const Chat = () => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    auth.signOut();
+    history.push('/');
+  };
+
   return (
-    <div>
-      <h1>Logged in!!!!</h1>
-      <h1>Chat page</h1>
-    </div>
+    <>
+      <header>
+        <div className='logo'>Salchat</div>
+        <div className='logout'>
+          <button onClick={handleLogout} className='btn btn-close'>
+            Logout
+          </button>
+        </div>
+      </header>
+      <main></main>
+    </>
   );
 };
 
