a75700c43d3d93e0b3d1e249ae141515e5f2a06e
Salem <nlsabm@yahoo.com>
2021-08-18 00:28:52 -0400

    signout auth

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -13,67 +13,12 @@ const Chat = () => {
     await auth.signOut();
     history.push('/');
   };
-
-  const getPhotofile = async (url) => {
-    const res = await fetch(url);
-    const data = await res.blob();
-    return new File([data], 'userPhoto.jpg', { type: 'image/jpeg' });
-  };
-
-  // // api handling user
-  // useEffect(() => {
-  //   if (!user) {
-  //     history.push('/');
-  //     return;
-  //   }
-  //   // get the user if user profile exists
-  //   axios
-  //     .get('https://api.chatengine.io/users/me', {
-  //       headers: {
-  //         'project-id': '97737ec9-d161-46c0-b3d3-eb567be2a080',
-  //         'user-name': user.email,
-  //         'user-secret': user.uid,
-  //       },
-  //     })
-  //     .then(() => {
-  //       setLoading(false);
-  //     })
-  //     .catch(() => {
-  //       let formdata = new FormData();
-  //       formdata.append('email', user.email);
-  //       formdata.append('username', user.email);
-  //       formdata.append('secret', user.uid);
-  //       getPhotofile(user.photoURL).then((ava) => {
-  //         formdata.append('avatar', ava, ava.name);
-  //         // if getting the image file was a success then creat user below
-  //         axios
-  //           .post('https://api.chatengine.io/users/', formdata, {
-  //             headers: {
-  //               'private-key': 'f8e7e582-fcea-4c2b-a531-96a1613a8e0f',
-  //             },
-  //           })
-  //           .then(() => {
-  //             setLoading(false);
-  //           })
-  //           .catch((err) => {
-  //             console.log(err);
-  //           });
-  //       });
-  //     });
-  // }, [user, history]);
-
-  // // when user logs in and the data is still loading show a message
-  // if (!user || loading) return 'loading...';
-
   // api handling
   useEffect(() => {
-    if (!user) {
-      history.push('/');
-    }
-
+    if (!user) return history.push('/');
     // get the user if user has chat engine profile
     axios
-      .get('https://api.chatengine.io/me', {
+      .get('https://api.chatengine.io/users/me', {
         headers: {
           'project-id': '97737ec9-d161-46c0-b3d3-eb567be2a080',
           'user-name': user.email,
@@ -86,6 +31,7 @@ const Chat = () => {
       })
       .catch(() => {
         // if new user does not have a chat engine profile then create one
+        if (!user || loading) return 'Loading.....';
         let formdata = new FormData();
         formdata.append('email', user.email);
         formdata.append('username', user.email);
@@ -101,10 +47,11 @@ const Chat = () => {
             setLoading(false);
           })
           .catch((err) => {
-            console.log(err);
+            console.log('err message is: ' + err);
           });
       });
-  }, [user, history]);
+  }, [history, user]);
+
   return (
     <>
       <header>
