3377a5e8554154bf7290f38c3588a114e319d048
Salem <nlsabm@yahoo.com>
2021-08-21 00:06:18 -0400

    login layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { auth } from '../fireauth/firebase';
 import firebase from 'firebase/app';
 import 'firebase/app';
-import { FontAwesomeIcon } from '@fortawesome/fontawesome-free';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab);
 const Login = () => {
   const handleLoginGoogle = () => {
     auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
@@ -12,20 +15,29 @@ const Login = () => {
     auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
   };
   return (
-    <div className='login-page'>
-      <div className='login-page-container'>
-        <h1 className='login-page-title'>Login</h1>
-        <div className='login-page-btns'>
-          <button onClick={handleLoginGoogle} className='btn'>
-            Sign in with Google
-          </button>
-          <FontAwesomeIcon icon={} />
-          <button onClick={handleLoginFacebook} className='btn'>
-            Sign in with Facebook
-          </button>
+    <>
+      <div className='login-page'>
+        <div className='login-page-header'>
+          <div className='login-page-header-logo'>SalChat</div>
+          <h1 className='login-page-header-headline'>
+            Your Space To Be Social
+          </h1>
+        </div>
+        <div className='login-page-container'>
+          <h1 className='login-page-title'>Login</h1>
+          <div className='login-page-btns'>
+            <button onClick={handleLoginGoogle} className='btn'>
+              <FontAwesomeIcon icon={['fab', 'google']} /> Sign in with Google
+            </button>
+            <i className='fab fa-facebook-square'></i>
+            <button onClick={handleLoginFacebook} className='btn'>
+              <FontAwesomeIcon icon={['fab', 'facebook']} /> Sign in with
+              Facebook
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
